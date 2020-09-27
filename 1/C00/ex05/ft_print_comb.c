94a6d428d32d507f822004ae0d865c17473e8bd8
AlexStefanos <alexandrestefanoss@gmail.com>
2020-09-27 12:49:25 +0200

    ex05 + notes exos

---
+++ 1/C00/ex05/ft_print_comb.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	char a;
+	char b;
+	char c;
+
+	a = '0';
+	b = '1';
+	c = '2';
+	while (a < b)
+	{
+		b = a + 1;
+		while (b < c)
+		{
+			c = b + 1;
+			while (c  <= '9')
+			{
+				ft_putchar(a);
+				ft_putchar(b);
+				ft_putchar(c);
+				if (!(a == '7' && b == '8' && c == '9'))
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+
+int main()
+{
+	ft_print_comb();
+}
