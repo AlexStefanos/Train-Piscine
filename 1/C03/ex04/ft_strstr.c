1603d7c5574f6217a0785ab325795dbf91d88136
AlexStefanos <alexandrestefanoss@gmail.com>
2020-11-06 15:18:48 +0100

    C03 sans ex05 'strlcat' et sans les main

--- 1/C03/ex04/ft_strstr.c
+++ 1/C03/ex04/ft_strstr.c
@@ -2,28 +2,20 @@ char	*ft_strstr(char *str, char *to_find)
 {
 	int i;
 	int j;
-	int save_i;
-	int len_to_find;
 
 	i = 0;
-	j = 0;
-	save_i = 0;
-	while (to_find[len_to_find])
-		len_to_find++;
+	if(to_find[0] == '\0')
+		return(str);
 	while (str[i])
 	{
-		if (str[i] == to_find[0])
+		j = 0;
+		while (to_find[j] == str[i + j])
 		{
-			save_i = i;
-			while (to_find[j] == str[i])
+			j++;
+			if(to_find[j] == '\0')
 			{
-				j++;
-				i++;
+				return(str + i);
 			}
-			if (j == len_to_find)
-				return (to_find); //verifier
-			j = 0;
-			i = save_i;
 		}
 		i++;
 	}
--- 1/C03/ex05/ft_strlcat.c
+++ 1/C03/ex05/ft_strlcat.c
@@ -1,4 +1,13 @@
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (dest[j])
+		j++;
+	while (src[i])
+		dest[j++] = src[i++];
+
 }
